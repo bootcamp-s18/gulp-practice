1b024d5b03fe319197e0dd67d2ad53402bb93172
Janine Hempy <jhempy@gmail.com>
2018-05-24 14:34:44 -0400

    Adds sass processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('copy', function() {
 	return gulp.src('app/**/*.+(htm|html)')
 		.pipe(gulp.dest('dist/'))
 })
+
+gulp.task('sass', function() {
+	return gulp.src('app/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css/'))
+})
+
