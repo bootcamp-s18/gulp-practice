8b8600efb3081bab62e77f8eeecfbe0ab7b1e3f2
Janine Hempy <jhempy@gmail.com>
2018-05-24 16:03:29 -0400

    WIP - not working

--- docs/requirements.md
+++ docs/requirements.md
@@ -3,10 +3,10 @@
 This is a lab project; we want to explore gulp.
 
 Try to use as many of its features as are practical:
-	* Move files
-	* Sass transpile to CSS
+	- Move files
+	- Sass transpile to CSS
 	* Watch to trigger tasks automatically
-	* Set up a default task
+	- Set up a default task
 	* Reload browser upon file change
 	* Merge files
 	* Minify files
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var inject = require('gulp-inject');
 var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
 
 // gulp.task('copyhtml', function() {
 // 	return gulp.src('app/**/*.+(htm|html)')
@@ -17,6 +18,9 @@ gulp.task('sass', function() {
 	return gulp.src('app/scss/**/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('dist/css/'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
 });
 
 gulp.task('inject', function() {
@@ -24,6 +28,20 @@ gulp.task('inject', function() {
 	var sources = gulp.src(['dist/css/**/*.css'], {read: false});
 	return targets.pipe(inject(sources))
 		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		},
+	})
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+	gulp.watch('app/**/*.+(htm|html)', ['inject']);
+	gulp.watch('app/css/**/*.css', ['copycss']);
+	gulp.watch('app/scss/**/*.scss', ['sass']);
 });
 
 gulp.task('default', function(callback) {
