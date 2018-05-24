b0faeb05adeb737dc00102f329143e851b224e69
Janine Hempy <jhempy@gmail.com>
2018-05-24 15:19:03 -0400

    Adds default task with injection

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var inject = require('gulp-inject');
+var runSequence = require('run-sequence');
 
-gulp.task('copy', function() {
-	return gulp.src('app/**/*.+(htm|html)')
-		.pipe(gulp.dest('dist/'))
-})
+// gulp.task('copyhtml', function() {
+// 	return gulp.src('app/**/*.+(htm|html)')
+// 		.pipe(gulp.dest('dist/'))
+// });
+
+gulp.task('copycss', function() {
+	return gulp.src('app/css/**/*.css')
+		.pipe(gulp.dest('dist/css/'))
+});
 
 gulp.task('sass', function() {
 	return gulp.src('app/scss/**/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('dist/css/'))
-})
+});
 
+gulp.task('inject', function() {
+	var targets = gulp.src('app/**/*.+(htm|html)');
+	var sources = gulp.src(['dist/css/**/*.css'], {read: false});
+	return targets.pipe(inject(sources))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', function(callback) {
+	runSequence(['sass', 'copycss'], 'inject', callback)
+});
